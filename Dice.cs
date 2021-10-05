a9b98e075f7f1ee08c3da48de3292af19f657f63
syedfaizz <syedfaizz45@gmail.com>
2021-10-05 17:00:36 +0530

    [faisal] Add: welcome

---
+++ Dice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class program
+{
+    static int findWays(int N)
+    {
+        if (N == 0)
+        {
+            return 1;
+        }
+
+        int cnt = 0;
+
+        for (int i = 1; i <= 6; i++)
+        {
+            if (N - i >= 0)
+            {
+                cnt = cnt + findWays(N - i);
+            }
+        }
+
+        return cnt;
+    }
+    public static void Main()
+    {
+        int N = 4;
+
+        Console.Write(findWays(N));
+    }
+}
